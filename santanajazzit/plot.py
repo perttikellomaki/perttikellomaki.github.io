d3cf6929591db32ea0ed11d33804f1e7b8d72276
Pertti Kellomaki <pertti.kellomaki@iki.fi>
2024-10-09 18:38:58 +0300

    Essu 9.10.2024

---
+++ santanajazzit/plot.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+import sys
+
+with open('data') as f:
+    raw_data = f.readlines()
+
+data = [l.split() for l in raw_data if len(l.split()) == 3]
+dogs = set([l[0] for l in data])
+
+for dog in dogs:
+    dates = [(datetime.datetime.strptime(l[1], '%Y-%m-%d') - datetime.datetime.strptime('2021-03-19', '%Y-%m-%d')).days
+             for l in data if l[0] == dog]
+    weights = [float(l[2]) for l in data if l[0] == dog]
+    plt.plot(dates, weights, label=dog)
+
+plt.legend()
+plt.xlim(0)
+plt.ylim(0)
+if len(sys.argv) > 1:
+    plt.savefig(sys.argv[1])
+else:
+    plt.show()
+
