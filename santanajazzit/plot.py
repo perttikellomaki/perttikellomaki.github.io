91860560071c7af50a878bf8547bf96e32431bc5
Pertti Kellomaki <pertti.kellomaki@iki.fi>
2024-10-09 18:51:14 +0300

    Santanajazzien syntymäpäivä

--- santanajazzit/plot.py
+++ santanajazzit/plot.py
@@ -11,7 +11,7 @@ data = [l.split() for l in raw_data if len(l.split()) == 3]
 dogs = set([l[0] for l in data])
 
 for dog in dogs:
-    dates = [(datetime.datetime.strptime(l[1], '%Y-%m-%d') - datetime.datetime.strptime('2021-03-19', '%Y-%m-%d')).days
+    dates = [(datetime.datetime.strptime(l[1], '%Y-%m-%d') - datetime.datetime.strptime('2024-08-20', '%Y-%m-%d')).days
              for l in data if l[0] == dog]
     weights = [float(l[2]) for l in data if l[0] == dog]
     plt.plot(dates, weights, label=dog)
